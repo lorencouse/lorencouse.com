7b1db0cae0c3a21f4388b2e81df38184f93fbed9
“Loren <couselm@gmail.com>
2024-12-23 14:52:30 +0800

    Changes to resume details.

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -4,10 +4,9 @@ import * as React from 'react';
 import useLoaded from '@/hooks/useLoaded';
 
 import Accent from '@/components/Accent';
-import CustomImg from '@/components/images/CustomImg';
+import SocialLinks from '@/components/links/SocialLinks';
 import ExperienceBlock from '@/components/resume/ExperienceBlock';
 import Seo from '@/components/Seo';
-import TechStack from '@/components/TechStack';
 
 export default function AboutPage() {
   const isLoaded = useLoaded();
@@ -47,11 +46,6 @@ export default function AboutPage() {
                   </article>
                 </div>
               </div>
-
-              <h3 className='mt-12'>Tech Stack</h3>
-              <figure className='mt-2'>
-                <TechStack />
-              </figure>
             </div>
           </div>
         </section>
@@ -64,34 +58,7 @@ export default function AboutPage() {
                 Feel free to contact me about my projects, current work, or web
                 development consultations. I’m happy to help.
               </p>
-            </article>
-          </div>
-        </section>
-
-        <section id='uses'>
-          <div className='layout mt-16'>
-            <h2>My Desk</h2>
-            <CustomImg
-              className='mt-8'
-              publicId='/images/about/loren-couse-desk.jpg'
-              width={1279}
-              height={913}
-              alt='Photo of my desk setup'
-            />
-            <article className='prose mt-4 dark:prose-invert'>
-              <ul>
-                <li>MacBook Air (13" M1 2020, 16GB, 1TB)</li>
-                <li>Asus 24 Inch Monitor (VZ249HE)</li>
-                <li>Logitech G610 Mechanical Keyboard</li>
-                <li>Asus MM511 Mouse</li>
-                <li>Neat Bumblebee II - Professional Condenser Mic</li>
-                <li>Google Nest Speaker</li>
-                <li>Kindle Fire Smart Display</li>
-                <li>Kindle Paperwhite (10th Generation)</li>
-                <li>Nintendo Switch Original</li>
-                <li>Apple iPhone 15 Pro Max</li>
-                <li>Beats Fit Pro Buds</li>
-              </ul>
+              <SocialLinks />
             </article>
           </div>
         </section>
@@ -117,7 +84,8 @@ const educationHistory: ExperienceProps[] = [
     locationLink: 'https://www.ncku.edu.tw/',
     city: 'Tainan, Taiwan',
     role: 'MBA - Information Technology',
-    description: 'Line 1\nLine 2\nLine 3',
+    description:
+      'Coursework in IT, cybersecurity, statistics, & data analytics.\nDatabase management systems and IT project management.\nResearch into digital marketing efficacy in e-commerce platforms.',
     img: '/images/resume/ncku-logo.jpeg',
   },
   {
@@ -150,7 +118,7 @@ const workHistory: ExperienceProps[] = [
     role: 'Market Research Coordinator',
     city: 'Melbourne, Australia',
     description:
-      'Conducted research: Analyzed market trends, competitor activities, and industry benchmarks to provide actionable insights for client strategies.\nPrepared reports: Compiled comprehensive presentations and reports highlighting key findings, market opportunities, and strategic recommendations.\nCollaborated with teams: Partnered with cross-functional teams to align research insights with client goals, ensuring data-driven decision-making.',
+      'Managed research field teams of 3-5 people, effectively executing research projects.\nOversaw field data collection, quality control, and project progress.\nAnalyzed market trends to align research insights with client goals.\nConducted new employee training and monthly performance evaluations.',
     img: '/images/resume/ey-logo.jpg',
   },
   {
@@ -160,7 +128,7 @@ const workHistory: ExperienceProps[] = [
     city: 'Kaohsiung, Taiwan',
     role: 'Startup Advisor',
     description:
-      'Provide strategic guidance on product development strategies, market positioning, and technology trends.\nSet goals and timelines based project targets to meet project and market demands.\nMentorship and Team Building, fostering a culture of innovation, effective leadership, and scalable team structures.\nFundraising and Networking Support to facilitate investor introductions, government grants, and strategic collaborations.',
+      'Provide strategic guidance on business development strategies, market positioning, and current trends.\nSet goals and timelines to meet project targets and investor expectations.\nMentored team building, effective leadership, and scalable team structures.\nFacilitated fundraising network events for investor introductions, government grants, and corporate collaborations.',
     img: '/images/resume/dakuo-logo.jpeg',
   },
 ];
